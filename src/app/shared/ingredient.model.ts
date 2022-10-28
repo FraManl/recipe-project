3a5ffe17c36ab6f2c406e70b284ebc7b1959aa4a
Francois Manlay <francois.manlay@gmail.com>
2022-10-28 15:40:04 +0200

    Building main components

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+  // Unecessary if public on constructor arguments
+  //   public name: string;
+  //   public amount: number;
+
+  constructor(public name: string, public amount: number) {
+    // Unecessary if public on constructor arguments
+    // this.name = name;
+    // this.amount = amount;
+  }
+}
