191030e38977ff05e2ccd28bb4f46a0309df8843
Francois Manlay <francois.manlay@gmail.com>
2022-11-02 17:58:04 +0100

    Added navigation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,20 +7,25 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
-      'Test Recipe',
-      'This is simply a test',
+      'Cassoulet aux fraises',
+      'Une belle recette dégueulasse',
       'https://eadn-wc02-3894996.nxedge.io/wp-content/uploads/2018/01/pistachio-turmeric-rice-bowl6-1024x683.jpg'
     ),
     new Recipe(
-      'Test Recipe',
-      'This is simply a test',
-      'https://eadn-wc02-3894996.nxedge.io/wp-content/uploads/2018/01/pistachio-turmeric-rice-bowl6-1024x683.jpg'
+      'Cordon bleu purée',
+      'Le traditionnel cordon avec sa purée au beurre',
+      'https://cdn.chefclub.tools/uploads/recipes/cover-thumbnail/41fd7230-01e0-4f35-aa96-2fb43894016c_a2dKvji.jpg'
     ),
   ];
 
   constructor() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 
   ngOnInit(): void {}
 }
