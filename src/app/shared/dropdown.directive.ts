4f8e7b6cf2586d36b7aacaf24e04fa4442e328d9
Francois Manlay <francois.manlay@gmail.com>
2022-11-03 22:54:13 +0100

    Added structural directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,13 +1,26 @@
-import { Directive, TemplateRef, ViewContainerRef } from '@angular/core';
+import {
+  Directive,
+  HostBinding,
+  HostListener,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Directive({ selector: '[appDropdown]' })
 
 // adds a css class on an element when it is clicked, and remove the class when clicked again
 export class DropdownDirect {
-  @Input() set appDropdown(condition: boolean) {}
+  //   @Input() set appDropdown(condition: boolean) {}
 
-  constructor(
-    private templateRef: TemplateRef<any>,
-    private vcRef: ViewContainerRef
-  ) {}
+  //   constructor(
+  //     private templateRef: TemplateRef<any>,
+  //     private vcRef: ViewContainerRef
+  //   ) {}
+
+  // attach an 'open' class to class array
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 }
