42510d9309912fe9c8f69810688bfacd8a53412f
Francois Manlay <francois.manlay@gmail.com>
2022-11-03 22:56:35 +0100

    Added structural directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -2,25 +2,26 @@ import {
   Directive,
   HostBinding,
   HostListener,
-  TemplateRef,
-  ViewContainerRef,
+  ElementRef,
 } from '@angular/core';
 
 @Directive({ selector: '[appDropdown]' })
 
 // adds a css class on an element when it is clicked, and remove the class when clicked again
 export class DropdownDirect {
-  //   @Input() set appDropdown(condition: boolean) {}
-
-  //   constructor(
-  //     private templateRef: TemplateRef<any>,
-  //     private vcRef: ViewContainerRef
-  //   ) {}
-
   // attach an 'open' class to class array
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  // this will only allow to close the tab by clicking on it
+  //   @HostListener('click') toggleOpen() {
+  //     this.isOpen = !this.isOpen;
+  //   }
+
+  // to make it closable when we click anywhere, do so
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
+  constructor(private elRef: ElementRef) {}
 }
