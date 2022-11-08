76902ab80e83935b2b24fae8ba125cf3937ac240
Francois Manlay <francois.manlay@gmail.com>
2022-11-08 21:56:31 +0100

    implementing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { Recipe } from './recipes/recipe.model';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'shopping-list', component: ShoppingListComponent },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: ':id', component: RecipeDetailComponent },
+      // { path: 'recipe-edit', component: RecipeDetailComponent },
+    ],
+  },
+  { path: '**', redirectTo: '/not-found', pathMatch: 'full' },
+];
 
+// NgModule will transform a normal typescript class into an angular module
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
