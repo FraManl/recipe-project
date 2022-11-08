22d49adb61bf120c692bb62920b00cc32b57c765
Francois Manlay <francois.manlay@gmail.com>
2022-11-08 22:53:52 +0100

    implementing routing

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -12,6 +12,7 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) {}
 
+  // angular natively clean up subscription after use (especially native observables such as Params), but always a good practice to unsubscribe custom observables explicitely in the code (thing we don't do here...); otherwise angular won't do it... cause not managed by angular
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
