b0ed4650e9ee0900cfbe8deec7ab2fa4d2bf0df6
Francois Manlay <francois.manlay@gmail.com>
2022-12-01 15:18:37 +0100

    Implementing Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppinglistModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
+    AuthModule,
     RecipesModule,
     ShoppinglistModule,
-    FormsModule,
-    ReactiveFormsModule,
     AppRoutingModule,
     SharedModule,
     CoreModule,
